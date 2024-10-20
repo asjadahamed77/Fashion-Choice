063c30cd81d7cd16b2d07bd49a22727ae13fd2cd
asjadahamed77 <asjadahamed77@gmail.com>
2024-10-20 17:37:08 +0530

    Completed Hero component and made some changes in Navbar

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import img1 from '../assets/Hero Images/hero1.jpg';
+import img2 from '../assets/Hero Images/hero2.jpg';
+import img3 from '../assets/Hero Images/hero3.jpg';
+import img4 from '../assets/Hero Images/hero4.jpg';
+
+const Hero = () => {
+
+  const images = [img1, img2, img3, img4];
+
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [images.length]);
+
+  return (
+    <div>
+      {/* ----- Hero Images -------- */}
+      <div className="relative">
+        {/* Display the current image */}
+        <img className="w-full h-[90vh] object-cover transition-opacity duration-1000" src={images[currentImageIndex]} alt="Hero" />
+
+        {/* Semi-transparent overlay */}
+        <div className="absolute inset-0 flex justify-center items-center bg-black opacity-55"></div>
+
+        {/* Text Container */}
+        <div className="absolute inset-0 flex flex-col justify-center text-white items-start w-[80%] mx-auto">
+          <h1 className="font-cursiveFont lg:text-8xl md:text-7xl sm:text-6xl text-4xl">
+            Define Your Style,<br /> Elevate Your Wardrobe
+          </h1>
+          <p className="font-headingFont w-[80%] md:text-lg mt-2 sm:text-base text-sm">
+            Discover the finest collections crafted for elegance, comfort, and sophistication. Your fashion, your choice.
+          </p>
+          <button className="bg-transparent sm:px-6 px-4 sm:py-2 py-1.5 text-sm sm:text-base mt-6 border-2 hover:text-black rounded font-headingFont hover:bg-white transition-all duration-300">
+            Explore Our Collections
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import logo from "../assets/Logo and Icons/Fashion Choice Logo.png";
 import fb_icon from "../assets/Logo and Icons/facebook.png";
 import insta_icon from "../assets/Logo and Icons/instagram.png";
 import whatsapp_icon from "../assets/Logo and Icons/whatsapp.png";
-import search from '../assets/Logo and Icons/search.png'
-import menu from '../assets/Logo and Icons/menu.png'
+import search from '../assets/Logo and Icons/search.png';
+import menu from '../assets/Logo and Icons/menu.png';
 
 const Navbar = () => {
-    const [showMenu,setShowMenu] = useState(false)
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <div>
       {/* Top navbar social icons */}
@@ -19,15 +20,16 @@ const Navbar = () => {
       </div>
 
       {/* Navbar Elements */}
-      <div className="flex items-center justify-around p-4 shadow-sm  border-gray-600">
+      <div className="flex items-center justify-around p-4 shadow-sm border-gray-600">
         {/* search */}
-        <div>
-            <img className="w-6 h-6 cursor-pointer" src={search} alt="" />
-        </div>
+        <Link to={'/search'}>
+          <img className="w-6 h-6 cursor-pointer" src={search} alt="Search icon" />
+        </Link>
+
         {/* Navbar Logo */}
-        <div>
+        <Link to={'/'}>
           <img className="lg:w-[300px] md:w-[250px] w-[250px]" src={logo} alt="Fashion Choice logo" />
-        </div>
+        </Link>
 
         {/* Navbar Links */}
         <ul className="hidden lg:flex items-center gap-6 lg:text-lg text-sm font-semibold">
@@ -82,67 +84,72 @@ const Navbar = () => {
             </NavLink>
           </li>
         </ul>
+
         {/* toggle menu */}
         <div>
-            <img onClick={() => setShowMenu(prev => !prev)} className={'lg:hidden w-8 h-8 cursor-pointer'} src={menu} alt="" />
+          <img onClick={() => setShowMenu(prev => !prev)} className="lg:hidden w-8 h-8 cursor-pointer" src={menu} alt="Menu icon" />
         </div>
       </div>
+
       {/* Mobile NavLinks */}
-    
-      <ul className={`font-semibold  ${showMenu ? 'max-h-[500px] translate-y-0 opacity-100' : 'max-h-0 translate-y-[-105%] opacity-0'} flex flex-col items-center transition-all duration-500 w-full shadow-md p-4 gap-4 overflow-hidden`}>
-
-
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? "text-blue-600" : "hover:text-gray-600"
-              }
-            >
-              HOME
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/collections"
-              className={({ isActive }) =>
-                isActive ? "text-blue-600" : "hover:text-gray-600"
-              }
-            >
-              COLLECTIONS
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/gallery"
-              className={({ isActive }) =>
-                isActive ? "text-blue-600" : "hover:text-gray-600"
-              }
-            >
-              GALLERY
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about-us"
-              className={({ isActive }) =>
-                isActive ? "text-blue-600" : "hover:text-gray-600"
-              }
-            >
-              ABOUT US
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact-us"
-              className={({ isActive }) =>
-                isActive ? "text-blue-600" : "hover:text-gray-600"
-              }
-            >
-              CONTACT US
-            </NavLink>
-          </li>
-        </ul>
+      <ul
+        className={`font-semibold z-[100] bg-white ${showMenu ? 'max-h-[500px] opacity-100' : 'max-h-0 opacity-0'} flex flex-col items-center transition-all duration-500 w-full shadow-md p-4 gap-4 overflow-hidden`}>
+        <li>
+          <NavLink
+            to="/"
+            onClick={() => setShowMenu(false)}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "hover:text-gray-600"
+            }
+          >
+            HOME
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/collections"
+            onClick={() => setShowMenu(false)}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "hover:text-gray-600"
+            }
+          >
+            COLLECTIONS
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/gallery"
+            onClick={() => setShowMenu(false)}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "hover:text-gray-600"
+            }
+          >
+            GALLERY
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about-us"
+            onClick={() => setShowMenu(false)}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "hover:text-gray-600"
+            }
+          >
+            ABOUT US
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/contact-us"
+            onClick={() => setShowMenu(false)}
+            className={({ isActive }) =>
+              isActive ? "text-blue-600" : "hover:text-gray-600"
+            }
+          >
+            CONTACT US
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
