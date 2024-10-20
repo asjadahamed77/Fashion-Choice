c6cf1a21bd2b20d9720738898366fcf436ba8de3
asjadahamed77 <asjadahamed77@gmail.com>
2024-10-20 11:11:16 +0530

    Created Pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'cursiveFont': ['"Dancing Script"', 'cursive'],
+        'headingFont': ['"Playfair Display"', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
