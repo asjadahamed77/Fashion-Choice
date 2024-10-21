8873bb1ef36918f5c395d92487ccefb87f3bc9c0
asjadahamed77 <asjadahamed77@gmail.com>
2024-10-21 19:30:21 +0530

    Updated Search

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -6,7 +6,7 @@ const Search = () => {
   return (
     <div className='w-full pb-8'>
       <div className='lg:w-[80%] sm:w-[85%] w-[90%] mx-auto border '>
-        <input value={search} onChange={(e)=>setSearch(e.target.value)} type="search" placeholder='search product' className='p-2 pl-4 border-none outline-none text-sm w-full' />
+        <input value={search} onChange={(e)=>setSearch(e.target.value)} type="search" placeholder='search product' className='p-2 pl-4 border-none bg-transparent outline-none text-sm w-full' />
       </div>
     </div>
   )
